847871984f9ca995b825f0d156c3e34f52ed9844
Anuvardhan <103085237+AnuvardhanBasam@users.noreply.github.com>
2025-10-01 22:24:19 -0500

    Update hello_data_engineer.py

--- day01/hello_data_engineer.py
+++ day01/hello_data_engineer.py
@@ -1,9 +1,8 @@
 # day01/hello_data_engineer.py
-# Tiny script for Day 1
-
 def main():
     print("Hello Data Engineer")
 
 if __name__ == "__main__":
     main()
-#day01 hello_data_engineer script
+
+# day01 hello_data_engineer script
