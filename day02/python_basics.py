f44f00ee711db262e068f2119f5e54b98d5085e5
Anuvardhan <103085237+AnuvardhanBasam@users.noreply.github.com>
2025-10-03 01:16:32 -0500

    Create python_basics.py
    day2 learning

---
+++ day02/python_basics.py
@@ -0,0 +1,7 @@
+#Day2 DataEng
+
+
+name = input("Enter your name: ")
+Age = int(input("Enter your age: "))
+Height = float(input("Enter your height: "))
+print("Hello", name + "!", "You are", Age, "years old and", Height, "meters tall.")  
