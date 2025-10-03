b4c9d78878feca3a25178b367d14d13dd6f17a5f
Anuvardhan <103085237+AnuvardhanBasam@users.noreply.github.com>
2025-10-03 01:06:40 -0500

    day2

---
+++ python_basics.py
@@ -0,0 +1,4 @@
+name = input("Enter your name: ")
+Age = int(input("Enter your age: "))
+Height = float(input("Enter your height: "))
+print("Hello", name + "!", "You are", Age, "years old and", Height, "meters tall.")
